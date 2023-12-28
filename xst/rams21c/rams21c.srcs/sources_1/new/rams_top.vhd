7c5e7357a1d35d41a70c372d85aa1adc8b21a322
Glenn Neidermeier <ded.reckning@gmail.com>
2023-12-28 18:45:47 -0500

    Update rams21c example

--- xst/rams21c/rams21c.srcs/sources_1/new/rams_top.vhd
+++ xst/rams21c/rams21c.srcs/sources_1/new/rams_top.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
--- Company: Red~Bote
--- Engineer: Red~Bote
+-- Company: 
+-- Engineer: 
 -- 
--- Create Date: 11/27/2023 02:56:21 PM
+-- Create Date: 12/28/2023 06:19:14 AM
 -- Design Name: 
 -- Module Name: rams_top - Behavioral
 -- Project Name: 
@@ -17,12 +17,14 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
+
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.all;
+--use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -30,45 +32,31 @@ use IEEE.NUMERIC_STD.all;
 --use UNISIM.VComponents.all;
 
 entity rams_top is
-    generic (constant CNTR_BITS : integer := 31);
-    port (
-        clk : in std_logic;
-        reset : in std_logic;
-        sw : in std_logic_vector (15 downto 0);
-        led : out std_logic_vector (15 downto 0));
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           sw : in STD_LOGIC_VECTOR (15 downto 0);
+           led : out STD_LOGIC_VECTOR (15 downto 0));
 end rams_top;
 
 architecture Behavioral of rams_top is
-    signal counter_out : integer range 0 to (2**CNTR_BITS)-1;
-    signal ram_address : std_logic_vector(5 downto 0);
+    signal ram_addr : std_logic_vector(5 downto 0);
     signal ram_data : std_logic_vector(19 downto 0);
 begin
-
-    u_counter : entity work.counters_8
-        generic map(MAX => (2 ** CNTR_BITS)-1)
-        port map(
-            C => clk,
-            CLR => reset, -- active high
-            Q => counter_out); -- integer output
-
-    clk_proc : process(clk)
-        variable counter_reg : unsigned (CNTR_BITS-1 downto 0);
-    begin
-        if rising_edge (clk)
-        then
-            counter_reg := to_unsigned(counter_out, counter_reg'length);
-            ram_address <= std_logic_vector(counter_reg(CNTR_BITS-1 downto 25));
-        end if;
-    end process clk_proc;
+    ram_addr <= sw(5 downto 0);
 
     u_rom : entity work.rams_21c
-        port map(
-            clk => clk,
-            en => '1',
-            addr => ram_address,
-            data => ram_data
-        );
+    port map (
+        clk => clk,
+        en => '1',
+        addr => ram_addr, -- in std_logic_vector(5 downto 0);
+        data => ram_data -- out std_logic_vector(19 downto 0));
+    );
 
-    led(15 downto 0) <= ram_data(15 downto 0);
+    out_proc : process(clk)
+    begin
+        if rising_edge(clk) then
+            led <= ram_data(15 downto 0);
+        end if;
+    end process out_proc;
 
 end Behavioral;
